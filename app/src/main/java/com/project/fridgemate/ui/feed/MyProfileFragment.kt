1aedc6cd16cc2f9bbb90a324a57b53da201be704
LAPTOP-90O3I2F7\yulik <yulikeren3@gmail.com>
2026-01-20 21:19:21 +0200

    added new screen

---
+++ app/src/main/java/com/project/fridgemate/ui/feed/MyProfileFragment.kt
@@ -0,0 +1,18 @@
+package com.project.fridgemate.ui.feed
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.fridgemate.R
+
+class MyProfileFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+}
